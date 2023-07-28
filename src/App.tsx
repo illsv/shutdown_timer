6f550670f75554468453137e9c5a92bb615cbbd3
Il <losev2710@gmail.com>
2023-07-28 15:14:12 +0300

    Add timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [ minutesAmount, setMinutesAmount ] = useState<number>(0);
   const [ timerActive, setTimerActive ] = useState<boolean>(false);
+  const [ secondsAmount, setSecondsAmount ] = useState<number>(0);
+
+  const [ hours, setHours ] = useState<number>(0);
+  const [ minutes, setMinutes ] = useState<number>(0);
+  const [ seconds, setSeconds ] = useState<number>(0);
+
+  useEffect(() => {
+    if(!secondsAmount) return
+    if(!timerActive) return
+
+    const interval = setInterval(() => {
+      const seconds = secondsAmount - 1
+      setSecondsAmount(seconds);
+      setReturnValues(seconds)
+    }, 1000);
+
+    return () => clearInterval(interval)
+  }, [secondsAmount, timerActive]);
+
 
   const toggleTimer = () => {
     setTimerActive(!timerActive)
   }
 
-  const addTimerMinutes = (amount: number) => {
-    setMinutesAmount(minutesAmount + amount)
+  const addMinutesToTimer = (amount: number) => {
+    const seconds = secondsAmount + (amount * 60)
+    setSecondsAmount(seconds)
+    setReturnValues(seconds)
   }
 
   const clearTimerMinutes = () => {
     setTimerActive(false)
-    setMinutesAmount(0)
+    setSecondsAmount(0)
+    setReturnValues(0)
   }
+
+  const setReturnValues = (seconds: number) => {
+    setHours(Math.floor(seconds / 60 / 60))
+    setMinutes(Math.floor(seconds / 60) % 60)
+    setSeconds(seconds % 60)
+  };
+
+  const formatTimeCell = (val: number) => {
+    return val ? val : '00'
+  }
+
 
 
   return (
@@ -34,20 +66,28 @@ function App() {
         </div>
 
         <div className="min-row">
-          <button type="button" onClick={ () => {addTimerMinutes(5)} }>5</button>
-          <button type="button" onClick={ () => {addTimerMinutes(10)} }>10</button>
-          <button type="button" onClick={ () => {addTimerMinutes(30)} }>30</button>
-          <button type="button" onClick={ () => {addTimerMinutes(60)} }>60</button>
-          <button type="button" onClick={ () => {addTimerMinutes(120)} }>120</button>
+          <button type="button" onClick={ () => {addMinutesToTimer(5)} }>5</button>
+          <button type="button" onClick={ () => {addMinutesToTimer(10)} }>10</button>
+          <button type="button" onClick={ () => {addMinutesToTimer(30)} }>30</button>
+          <button type="button" onClick={ () => {addMinutesToTimer(60)} }>60</button>
+          <button type="button" onClick={ () => {addMinutesToTimer(120)} }>120</button>
         </div>
 
         <button type="button" id='clear-timer' onClick={clearTimerMinutes}>Clear</button>
       </form>
 
-    <p>Timer Minutes: {minutesAmount}</p>
-    <p>{'Timer is ' + (timerActive ? 'ON' : 'OFF') + '.'}</p>
+      <div className="countdown">
+        <div className="hours">{formatTimeCell(hours)}</div>
+        <div className="time-divider">:</div>
+        <div className="minutes">{formatTimeCell(minutes)}</div>
+        <div className="time-divider">:</div>
+        <div className="seconds">{formatTimeCell(seconds)}</div>
+      </div>
 
-    <button type="button" id='toggle-timer' onClick={toggleTimer}>{ timerActive ? 'Pause' : 'Start' }</button>
+      {/* <p>SecondsAmount: {secondsAmount}</p> */}
+      {/* <p>{'Timer is ' + (timerActive ? 'ON' : 'OFF') + '.'}</p> */}
+
+      <button type="button" onClick={toggleTimer} disabled={!secondsAmount}>{ timerActive ? 'Pause' : 'Start' }</button>
     </div>
   );
 }
